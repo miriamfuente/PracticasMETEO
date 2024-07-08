e703e0e28a6aaaa43f25b3a3ddd79f0537fa8540
miriamfuente <146744820+miriamfuente@users.noreply.github.com>
2024-07-08 17:31:33 +0200

    a couple of bugs have been fixed

--- Code/compound_events_abs.R
+++ Code/compound_events_abs.R
@@ -6,14 +6,26 @@
 options(java.parameters = "-Xmx32g")
 library(loadeR)
 library(transformeR)
+library(visualizeR)
+library(climate4R.UDG)
+library(dplyr)
+library(climate4R.value)
+library(VALUE)
+library(gridExtra)
+library(grid)
 
 # Define the variables to process the data
-pr <- readRDS("/oceano/gmeteo/users/fuentesm/AEMET/TFM/section4.1/pr.obs.DD.1982-2021.rds")
-tmax <- readRDS("/oceano/gmeteo/users/fuentesm/AEMET/TFM/section4.1/tasmax.obs.DD.1982-2021.rds")
-quantile_pr <- 0.90
-quantile_tmax <- 0.10
-years <- 1982:2021
+pr <- readRDS("/lustre/gmeteo/WORK/fuentesm/TFM/section4.1/pr.obs.DD.1971-2020.rds")
+tmax <- readRDS("/lustre/gmeteo/WORK/fuentesm/TFM/section4.1/tasmax.obs.DD.1971-2020.rds")
+years <- 1971:1996
+
+pr <- subsetGrid(pr, years =years)
+tmax <- subsetGrid(tmax, years = years)
+pr_abs <- 0.1 # mm/dia
+tmax_abs <- 30 # ºC
 seasons <- list(Winter = c(12, 1, 2), Spring = c(3, 4, 5), Summer = c(6, 7, 8), Autumn = c(9, 10, 11))
+earth.layer <- climatology(tmax)
+earth.layer$Data[!is.na(earth.layer$Data)] <- 1
 
 processed_data <- function(data){
     # Function to obtain the data by season and year
@@ -90,8 +102,8 @@ binarization <- function(grid_data, pr, tmax, variable) {
     return(grid_data)
 }
 
-pr_bin <- binarization(pr, quantile_pr, quantile_tmax, "pr")
-tmax_bin <- binarization(tmax, quantile_pr, quantile_tmax ,"tmax")
+pr_bin <- binarization(pr, pr_abs, tmax_abs, "pr")
+tmax_bin <- binarization(tmax, pr_abs, tmax_abs ,"tmax")
 
 # Create a function that iter over the years and seasons of two datagrids and execute the function gridArithmetics
 compound_event <- function(grid1, grid2) {
@@ -115,6 +127,7 @@ compound_event <- function(grid1, grid2) {
 
 CE <- compound_event(pr_bin, tmax_bin)  
 
+
 # Bind the data of a specific station for several years
 bindGrid_stations <- function(data, season) {
     # Extract the years available in the data
@@ -133,13 +146,10 @@ bindGrid_stations <- function(data, season) {
     return(result)
 }
 
-earth.layer <- climatology(tmax)
-earth.layer$Data[!is.na(earth.layer$Data)] <- 1
-
 # Name of the seasons
 season_names <- c("Winter", "Spring", "Summer", "Autumn")
 # List to store the results
-dataPeriod <- list()
+num.CE.period <- list()
 # Iterate over the seasons
 for (season in season_names) {
     # Obtain the data for the current season
@@ -148,6 +158,62 @@ for (season in season_names) {
     print("Done")
     data_season <- gridArithmetics(data_season, earth.layer, operator = "*")
     # Almacenar los resultados en la lista
-    dataPeriod[[estacion]] <- data_season
+    num.CE.period[[season]] <- data_season
 }
 
+pdf(paste0("num.CE.season", years, ".pdf"), width=9, height=12)
+spatialPlot(makeMultiGrid(num.CE.period$Winter, num.CE.period$Spring, num.CE.period$Summer, num.CE.period$Autumn, skip.temporal.check=TRUE), 
+            backdrop.theme = "coastline", as.table=TRUE, names.attr=c("Invierno", "Primavera", "Verano", "Otoño"),
+            color.theme="RdBu", rev.colors=TRUE, main="Eventos Extremos Compuestos por estación (1971-1996)",
+            layout=c(2,2),
+            colorkey = list(space = "bottom",
+                title = list("Nº de días", cex = 1)))
+dev.off()
+
+##########
+# TRENDS #
+##########
+winter <- bindGrid_stations(CE, season = "Winter")
+spring <- bindGrid_stations(CE, season = "Spring")
+summer <- bindGrid_stations(CE, season = "Summer")
+autumn <- bindGrid_stations(CE, season = "Autumn")
+
+trend.winter <- valueIndex(winter, index.code = "Trend", return.NApercentage = FALSE)# La pendiente
+sig.winter <- valueIndex(winter, index.code = "TrendSig", return.NApercentage = FALSE) # La significancia (1 o 0)
+clim.w = climatology(sig.winter)
+points.winter <- visualizeR::map.stippling(clim.w, thereshold = 0.05, condition = "LT", pch = 19, cex = 0.05, col = "black")
+plot.winter <- spatialPlot(trend.winter, backdrop.theme = "coastline", color.theme = "RdBu", at = seq(-0.1, 0.1, by = 0.01), 
+                set.max = 0.1, set.min = -0.1, rev.colors = TRUE, main = "Invierno", sp.layout = list(points.winter),
+                colorkey=list(space = "bottom", title = list("Nº de días/año", cex = 1)))
+
+trend.spring <- valueIndex(spring, index.code = "Trend", return.NApercentage = FALSE)# La pendiente
+sig.spring <- valueIndex(spring, index.code = "TrendSig", return.NApercentage = FALSE) # La significancia (1 o 0)
+clim.spring = climatology(sig.spring)
+points.spring <- visualizeR::map.stippling(clim.spring, thereshold = 0.05, condition = "LT", pch = 19, cex = 0.05, col = "black")
+plot.spring <- spatialPlot(trend.spring, backdrop.theme = "coastline", color.theme = "RdBu", at = seq(-0.1, 0.1, by = 0.01),
+                 set.max = 0.1, set.min = -0.1, rev.colors = TRUE, main = "Primavera", sp.layout = list(points.spring),
+                 colorkey=list(space = "bottom", title = list("Nº de días/año", cex = 1)))
+
+trend.summer <- valueIndex(summer, index.code = "Trend", return.NApercentage = FALSE)# La pendiente
+sig.summer <- valueIndex(summer, index.code = "TrendSig", return.NApercentage = FALSE) # La significancia (1 o 0)
+clim.summer = climatology(sig.summer)
+points.summer <- visualizeR::map.stippling(clim.summer, thereshold = 0.05, condition = "LT", pch = 19, cex = 0.05, col = "black")
+plot.summer <- spatialPlot(trend.summer, backdrop.theme = "coastline", color.theme = "RdBu", at = seq(-0.1, 0.1, by = 0.01),
+                 set.max = 0.1, set.min = -0.1, rev.colors = TRUE, main = "Verano", sp.layout = list(points.summer),
+                 colorkey=list(space = "bottom", title = list("Nº de días/año", cex = 1)))
+
+trend.autumn <- valueIndex(autumn, index.code = "Trend", return.NApercentage = FALSE)# La pendiente
+sig.autumn <- valueIndex(autumn, index.code = "TrendSig", return.NApercentage = FALSE) # La significancia (1 o 0)
+clim.autumn = climatology(sig.autumn)
+points.autumn <- visualizeR::map.stippling(clim.autumn, thereshold = 0.05, condition = "LT", pch = 19, cex = 0.05, col = "black")
+plot.autumn <- spatialPlot(trend.autumn, backdrop.theme = "coastline", color.theme = "RdBu", at = seq(-0.1, 0.1, by = 0.01),
+                 set.max = 0.1, set.min = -0.1, rev.colors = TRUE, main = "Otoño", sp.layout = list(points.autumn),
+                 colorkey=list(space = "bottom", title = list("Nº de días/año", cex = 1)))
+
+# Create the PDF file
+pdf(paste0("trend.CE.season",years,".pdf"), width=9, height=8)
+main.title <- textGrob("Tendencias de los eventos extremos compuestos por estación (1971-1996)",
+                        gp=gpar(cex=1.5, fontface="bold"))
+grid.arrange(plot.winter, plot.spring, plot.summer, plot.autumn,
+            ncol = 2, top = main.title)
+dev.off()
